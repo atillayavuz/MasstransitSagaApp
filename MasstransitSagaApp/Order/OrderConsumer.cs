0aa3f4a0cb844128e6558eaf08d75d135c1fac94
Atilla YAVUZ <atillayavuz@gmail.com>
2019-01-15 21:19:35 +0300

    OrderConsumer updated

---
+++ MasstransitSagaApp/Order/OrderConsumer.cs
@@ -0,0 +1,16 @@
+﻿using Contracts;
+using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace Order
+{
+    public class OrderConsumer : IConsumer<SendOrder>
+    {
+        public Task Consume(ConsumeContext<SendOrder> context)
+        {
+            Console.WriteLine($"Order with {context.Message.Id} accepted.");
+            return Task.FromResult(0);
+        }
+    } 
+}
--- MasstransitSagaApp/Order/Program.cs
+++ MasstransitSagaApp/Order/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MassTransit;
+using System;
 
 namespace Order
 {
@@ -6,6 +7,20 @@ namespace Order
     {
         static void Main(string[] args)
         {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(config =>
+            {
+                var host = config.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("user");
+                    h.Password("bitnami");
+                });
+
+                config.ReceiveEndpoint(host, "notification-queue", ep => {
+                    ep.Consumer<OrderConsumer>();
+                });
+            });
+            busControl.Start();
+
             Console.WriteLine("Hello World!");
         }
     }
