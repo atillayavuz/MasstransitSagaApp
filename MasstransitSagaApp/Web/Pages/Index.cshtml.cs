b4b8c785e7833619c22ad4a4813573e630afdb6b
Atilla YAVUZ <atillayavuz@gmail.com>
2019-01-15 21:20:26 +0300

    Masstransit producer configuration

--- MasstransitSagaApp/Web/Pages/Index.cshtml.cs
+++ MasstransitSagaApp/Web/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,14 @@ namespace Web.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IBus _bus;
+        public IndexModel(IBus bus)
+        {
+            _bus = bus;
+        }
         public void OnGet()
         {
-
+            _bus.Publish<SendOrder>(new SendOrder() { Id = 1 });
         }
     }
 }
--- MasstransitSagaApp/Web/Startup.cs
+++ MasstransitSagaApp/Web/Startup.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using GreenPipes;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -29,9 +32,21 @@ namespace Web
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
-
+             
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSingleton(provider => Bus.Factory.CreateUsingRabbitMq(
+                  cfg =>
+                  {
+                      var host = cfg.Host(new Uri(Configuration.GetSection("RabbitMqHost").Value), h =>
+                      {
+                          h.Username(Configuration.GetSection("RabbitMqUsername").Value);
+                          h.Password(Configuration.GetSection("RabbitMqPassword").Value);
+                      }); 
+                  }));
+
+            services.AddSingleton<IBus>(provider => provider.GetRequiredService<IBusControl>());
+            services.AddSingleton<Microsoft.Extensions.Hosting.IHostedService, MassTransitHostedService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,10 +61,30 @@ namespace Web
                 app.UseExceptionHandler("/Error");
             }
 
+
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
             app.UseMvc();
+        }
+        public class MassTransitHostedService : Microsoft.Extensions.Hosting.IHostedService
+        {
+            private readonly IBusControl busControl;
+
+            public MassTransitHostedService(IBusControl busControl)
+            {
+                this.busControl = busControl;
+            }
+
+            public async Task StartAsync(CancellationToken cancellationToken)
+            {
+                await busControl.StartAsync(cancellationToken);
+            }
+
+            public async Task StopAsync(CancellationToken cancellationToken)
+            {
+                await busControl.StopAsync(cancellationToken);
+            }
         }
     }
 }
